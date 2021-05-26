205ced7c34f5d6f2352462021eabb0e836f48f7a
Melissa Guachun <1212malka@gmail.com>
2021-05-26 13:15:18 -0400

    commit tree f9ffa5983fb9b56bd33f374092baca2ebda13c88

--- db/migrate/20210526170918_add_artist_id_to_songs.rb
+++ db/migrate/20210526170918_add_artist_id_to_songs.rb
@@ -1,5 +1,8 @@
 class AddArtistIdToSongs < ActiveRecord::Migration[5.0]
   def change
-    add_column :songs, :artist_id, :integer
+    change_table :songs do |t|
+      t.integer :artist_id
+    end
+    
   end
 end
